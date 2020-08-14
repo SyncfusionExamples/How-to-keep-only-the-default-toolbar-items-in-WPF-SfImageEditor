1d601f57bc716bd8dd68b09ef179531217ee0414
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-08-14 13:38:09 +0530

    Sample included

--- ImageEditor_GettingStarted/MainWindow.xaml.cs
+++ ImageEditor_GettingStarted/MainWindow.xaml.cs
@@ -1,26 +1,39 @@
-# How-to-keep-only-the-default-toolbar-items-in-WPF-SfImageEditor
+﻿using Syncfusion.UI.Xaml.ImageEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-This article explains how to disable the toolbar items except the default toolbar items such as text, shape, rotate, flip, crop and path in Syncfusion WPF SfImageEditor. 
+namespace ImageEditor_GettingStarted
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
 
-It can be achieved by using the custom image editor and overriding the OnApplyTemplate method to disable the toolbar items as shown in the following code snippet.
-
-**[XAML]**
-
-```
-        <local:CustomEditor x:Name="editor" ImageSource="CustomViewImage1.png">
-        </local:CustomEditor>
-```        
-
-**[C#]**
-
-```
     public class CustomEditor : SfImageEditor
     {
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
             var items = this.ToolbarSettings.ToolbarItems.Count;
-            var border = this.Template.FindName("PART_HeaderToolbarPanel", this) as       Border;
+            var border = this.Template.FindName("PART_HeaderToolbarPanel", this) as Border;
             var panel = (border.Child as Grid).Children[0] as StackPanel;
             var browseButton = panel.Children[0] as Button;
             var saveButton = panel.Children[1] as Button;
@@ -65,14 +78,4 @@ It can be achieved by using the custom image editor and overriding the OnApplyTe
             text.Visibility = Visibility.Collapsed;
         }
     }
-```
-**Output:**
-![](Output.png)
- 
-**See also**
-
-[How to enable/disable the toolbar visibility](https://help.syncfusion.com/wpf/image-editor/toolbar-customization#visibility)
-
-[How to add the additional items to the toolbar](https://help.syncfusion.com/wpf/image-editor/toolbar-customization#add-a-item)
-
-[How to customize the height of toolbar](https://help.syncfusion.com/wpf/image-editor/toolbar-customization#customization-1)
+}
